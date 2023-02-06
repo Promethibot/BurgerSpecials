ba888159760fb84911935162c6d82549c097650a
Promethibot <paola.mancini@me.com>
2023-02-06 02:34:29 -0500

    Add project files.

---
+++ BurgerSpecials/frmBurgers.vb
@@ -0,0 +1,9 @@
+﻿Public Class frmBurgers
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles lblHeading.Click
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles lblConfirmation.Click
+
+    End Sub
+End Class
